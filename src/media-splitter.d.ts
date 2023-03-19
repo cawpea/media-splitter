01b2b872df055fce7388495f02fbbb23b02ecfd6
m.osumi <m.osumi@goodpatch.com>
2023-03-19 15:10:10 +0900

    :shower: delete unnecessary files

--- src/media-splitter.d.ts
+++
@@ -1,2 +0,0 @@
-import { SplitMediaProps } from "./types";
-export declare const splitMedia: ({ inputFile, outputDir, outputFileName, splitDurationMs, }: SplitMediaProps) => void;
--- src/types.d.ts
+++
@@ -1,6 +0,0 @@
-export type SplitMediaProps = {
-    inputFile: string;
-    outputDir: string;
-    outputFileName: (index: number, defaultName: string) => string;
-    splitDurationMs: number;
-};
